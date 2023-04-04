5825a33e74f6f1771e8e22cfe5510c27352abd37
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-04-04 22:57:46 +0200

    Extract RangeSlider component

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,7 @@
     </div>
 
     <form class="password-form">
-      <div class="range-slider">
-        <p class="length-container">Password length
-          <span class="length-container__value">0</span>
-        </p>
-        <input class="range-slider__range" type="range" value="100" min="0" max="500">
-      </div>
+      <RangeSlider />
 
       <input type="text" name="custom-password-input" id="custom-password-input" placeholder="Write your custom password">
 
@@ -39,70 +34,22 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import RangeSlider from '@/components/RangeSlider.vue'
+
+export default {
+  components: {
+    RangeSlider,
+  },
+}
+
+</script>
 
 <style scoped>
 .app-title {
   margin: 1rem 0;
   text-align: center;
 }
-
-.slider-container {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-}
-
-.slider-bg {
-  position: absolute;
-  top: 50%;
-  width: 100%;
-  height: 15px;
-  border-radius: 5px;
-  background: #d3d3d3;
-  z-index: -1;
-}
-
-.slider {
-  -webkit-appearance: none;
-  appearance: none;
-  background: transparent;
-  height: 15px;
-  outline: none;
-  opacity: 0.7;
-  -webkit-transition: .2s;
-  transition: opacity .2s;
-}
-
-.slider:hover {
-  opacity: 1;
-}
-
-
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background-color: #04AA6D;
-  cursor: pointer;
-}
-
-.slider::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #04AA6D;
-  cursor: pointer;
-}
-
-.length-container {
-  display: flex;
-  justify-content: space-between;
-}
-
 
 .result-container {
   display: flex;
---
+++ src/components/RangeSlider.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="range-slider">
+    <p class="length-container">Password length
+      <span class="length-container__value">0</span>
+    </p>
+    <input class="range-slider__range" type="range" value="100" min="0" max="500">
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+.length-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.range-slider {
+  /* // Base Colors */
+  --shade-10: var(--black-color);
+  --shade-1: var(--white-color);
+  --shade-0: var(--white-color);
+  --teal: teal;
+
+  /* // Range Slider */
+  --range-handle-color: var(--shade-10);
+  --range-handle-color-hover: var(--teal);
+  --range-handle-size: 20px;
+
+  --range-track-color: var(--shade-1);
+  --range-track-height: 10px;
+
+  --range-label-color: var(--shade-10);
+  --range-label-width: 60px;
+
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
+
+.range-slider__range {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: var(--range-track-height);
+  border-radius: 5px;
+  background: var(--range-track-color);
+  outline: none;
+  padding: 0;
+  margin: 0;
+}
+
+/* // Range Handle */
+.range-slider__range::-webkit-slider-thumb {
+  appearance: none;
+  width: var(--range-handle-size);
+  height: var(--range-handle-size);
+  border-radius: 50%;
+  background: var(--range-handle-color-hover);
+  cursor: pointer;
+  transition: background 0.15s ease-in-out;
+
+
+}
+
+.range-slider__range::-webkit-slider-thumb:hover {
+  background: var(--range-handle-color);
+}
+
+
+.range-slider__range:active::-webkit-slider-thumb {
+  background: var(--range-handle-color);
+}
+
+.range-slider__range::-moz-range-thumb {
+  width: var(--range-handle-size);
+  height: var(--range-handle-size);
+  border: 0;
+  border-radius: 50%;
+  background: var(--range-handle-color);
+  cursor: pointer;
+  transition: background 0.15s ease-in-out;
+}
+
+.range-slider__range::-moz-range-thumb:hover {
+  background: var(--range-handle-color-hover);
+}
+
+
+.range-slider__range:active::-moz-range-thumb {
+  background: var(--range-handle-color-hover);
+}
+
+/* // Focus state */
+.range-slider__range:focus::-webkit-slider-thumb {
+  box-shadow: 0 0 0 3px var(--shade-0), 0 0 0 6px var(--teal);
+}
+
+/* // Firefox Overrides */
+::-moz-range-track {
+  background: var(--range-track-color);
+  border: 0;
+}
+
+input::-moz-focus-inner,
+input::-moz-focus-outer {
+  border: 0;
+}
+</style>
