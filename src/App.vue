7b9fbaed9e159c1630720e6cfeefaf72f41b9e6f
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-04-07 23:36:49 +0200

    Center container Scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="container">
     <h1 class="app-title">Password Generator</h1>
-
     <div class="result-container">
       <p class="result">
         {{ password }}
       </p>
     </div>
-
     <form @submit.prevent="generate" class="password-form">
       <!-- Si custom-password esta checked entonces se vera esto y sino lo otro que es por defecto -->
       <!-- <CheckBox label="Custom password" id="custom-password" /> -->
       <!-- <input type="text" name="custom-password-input" id="custom-password-input" placeholder="Write your custom password">
-        <CheckBox label="Fill spaces with symbols" id="fill-spaces" /> -->
-
+          <CheckBox label="Fill spaces with symbols" id="fill-spaces" /> -->
       <RangeSlider />
       <CheckBox label="Allow lowercase" id="hasLowercase" checked />
       <CheckBox label="Allow uppercase" id="hasUppercase" checked />
       <CheckBox label="Allow numbers" id="hasNumbers" checked />
       <CheckBox label="Allow symbols" id="hasSymbols" checked />
-
       <div class="btn-container">
         <button>
           Generate
@@ -74,6 +70,7 @@ export default {
   padding: 1rem;
   color: var(--black-color);
   font-weight: 700;
+  overflow: auto;
 }
 
 .result {
